ca04f2feb2be2a6204544e975f97e9696fc871b2
mjura <Mihaela.Jura@endava.com>
2019-03-22 15:29:01 +0200

    java core crud Spring

---
+++ src/main/java/com/endava/demo/service/InternService.java
@@ -0,0 +1,17 @@
+package com.endava.demo.service;
+
+import com.endava.demo.entity.Intern;
+
+import java.util.List;
+
+public interface InternService {
+    public List<Intern> getAllInterns();
+
+    public void add(Intern intern);
+
+    public void remove(int id);
+
+    Intern getInternById(int id);
+
+    void update(Intern intern);
+}
